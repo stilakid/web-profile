3fdbfba2bb07912da4495ee6c297c9dbc81b7b6b
Juben Rana <stilakid@stanford.edu>
2023-09-01 11:42:17 +0600

    uploading videos.js

---
+++ src/Assets/Videos/videos.js
@@ -0,0 +1,60 @@
+const videos = {
+
+    projects: {
+        general: {
+        },
+        bunnyWorld: {
+            scriptManager_1: require('./Projects/BunnyWorld/Vid_1.mp4'),
+            scriptManager_2: require('./Projects/BunnyWorld/Vid_2.mp4'),
+            scriptManager_3: require('./Projects/BunnyWorld/Vid_3.mp4'),
+            scriptManager_4: require('./Projects/BunnyWorld/Vid_4.mp4'),
+            scriptManager_5: require('./Projects/BunnyWorld/Vid_5.mp4'),
+            scriptManager_6: require('./Projects/BunnyWorld/Vid_6.mp4'),
+            scriptManager_7: require('./Projects/BunnyWorld/Vid_7.mp4'),
+            scriptManager_8: require('./Projects/BunnyWorld/Vid_8.mp4'),
+            scriptManager_9: require('./Projects/BunnyWorld/Vid_9.mp4'),
+            scriptManager_10: require('./Projects/BunnyWorld/Vid_10.mp4'),
+            scriptManager_11: require('./Projects/BunnyWorld/Vid_11.mp4'),
+        },
+        sura: {
+            cms_1: require('./Projects/Sura/main-1.mp4'),
+            cms_2: require('./Projects/Sura/main-2.mp4'),
+            cms_3: require('./Projects/Sura/main-3.mp4'),
+            cms_4: require('./Projects/Sura/main-4.mp4'),
+            cms_5: require('./Projects/Sura/main-5.mp4'),
+            cms_6: require('./Projects/Sura/main-6.mp4'),
+            cms_7: require('./Projects/Sura/main-7.mp4'),
+            cms_8: require('./Projects/Sura/main-8.mp4'),
+            cms_9: require('./Projects/Sura/main-9.mp4'),
+            cms_10: require('./Projects/Sura/main-10.mp4'),
+            cms_11: require('./Projects/Sura/main-11.mp4'),
+            cms_12: require('./Projects/Sura/main-12.mp4'),
+            cms_13: require('./Projects/Sura/main-13.mp4'),
+            cms_14: require('./Projects/Sura/main-14.mp4'),
+            cms_15: require('./Projects/Sura/main-15.mp4'),
+            cms_16: require('./Projects/Sura/main-16.mp4'),
+            cms_17: require('./Projects/Sura/main-17.mp4'),
+            cms_18: require('./Projects/Sura/main-18.mp4'),
+            cms_19: require('./Projects/Sura/main-19.mp4'),
+            cms_20: require('./Projects/Sura/main-20.mp4'),
+        },
+    },
+
+    hobbies: {
+        rockets: {
+            ssi1: require('./Hobbies/Rockets/ssi1.mov'),
+            ssi2: require('./Hobbies/Rockets/ssi2.mp4')
+        },
+
+        videoEditing: {
+            amv: require('./Hobbies/VideoEditing/amv.mp4'),
+            news1: require('./Hobbies/VideoEditing/news_1.mp4'),
+            news2: require('./Hobbies/VideoEditing/news_2.mp4'),
+            news3: require('./Hobbies/VideoEditing/news_3.mp4'),
+            news4: require('./Hobbies/VideoEditing/news_4.mp4'),
+            dance: require('./Hobbies/VideoEditing/dance.mp4'),
+        }
+    }
+}
+
+export default videos;
