25b6724f11c2086f773289d2d9e8af8c53d0fe13
Juben Rana <stilakid@stanford.edu>
2023-09-01 10:48:26 +0600

    uploading image js

---
+++ src/Assets/Images/images.js
@@ -0,0 +1,110 @@
+const images = {
+
+    home: {
+        juben: {
+            profilePic: require('./Home/profile_pic.jpeg'),
+            danceWod: require('./Home/dance_wod.jpg'),
+            musicLsjumb: require('./Home/music_lsjumb.jpg'),
+            bhutan: require('./Home/bhutan_aaron-santelices.jpg'),
+            ssi: require('./Home/ssi.jpg')
+        }
+    },
+
+    projects: {
+        general: {
+        },
+        wallus: {
+            preview: require('./Projects/Wallus/preview.png'),
+            qrCode: require('./Projects/Wallus/expo_go_qr_code.svg').default,
+        },
+        bunnyWorld: {
+            preview: require('./Projects/BunnyWorld/preview.png'),
+        },
+        sura: {
+            preview: require('./Projects/Sura/preview.png'),
+        },
+        webProfile: {
+            preview: require('./Projects/WebProfile/preview.png'),
+        },
+        carta: {
+            preview: require('./Projects/Carta/preview.png'),
+            class2021: require('./Projects/Carta/2021.png'),
+            class2022: require('./Projects/Carta/2022.png'),
+            graph1: require('./Projects/Carta/graph_1.png'),
+            graph2: require('./Projects/Carta/graph_2.png'),
+            graph3: require('./Projects/Carta/graph_3.png'),
+        },
+        blender: {
+            preview: require('./Projects/Blender/preview.png'),
+            finalImage: require('./Projects/Blender/final_image.png'),
+            variantA: require('./Projects/Blender/variant_a.png'),
+            variantB: require('./Projects/Blender/variant_b.png'),
+            variantC: require('./Projects/Blender/variant_c.png'),
+            motivationalImage1: require('./Projects/Blender/motivational_image_1.jpg'),
+            motivationalImage2: require('./Projects/Blender/motivational_image_2.jpg'),
+            motivationalImage3: require('./Projects/Blender/motivational_image_3.jpg'),
+            motivationalImage4: require('./Projects/Blender/motivational_image_4.jpg'),
+            motivationalImage5: require('./Projects/Blender/motivational_image_5.jpg'),
+            bridge1: require('./Projects/Blender/bridge_1.png'),
+            bridge2: require('./Projects/Blender/bridge_2.png'),
+            asphalt1: require('./Projects/Blender/asphalt_1.png'),
+            asphalt2: require('./Projects/Blender/asphalt_2.png'),
+            asphalt3: require('./Projects/Blender/asphalt_3.png'),
+            asphalt4: require('./Projects/Blender/asphalt_4.png'),
+            barrier: require('./Projects/Blender/barrier.png'),
+            streetlight1: require('./Projects/Blender/streetlight_1.png'),
+            streetlight2: require('./Projects/Blender/streetlight_2.png'),
+            streetRailing: require('./Projects/Blender/street_railing.png'),
+            pavement1: require('./Projects/Blender/pavement_1.png'),
+            pavement2: require('./Projects/Blender/pavement_2.png'),
+            embeddedRoadLight: require('./Projects/Blender/embedded_road_light.png'),
+            car1: require('./Projects/Blender/car_1.png'),
+            car2: require('./Projects/Blender/car_2.png'),
+            smoke1: require('./Projects/Blender/smoke_1.png'),
+            smoke2: require('./Projects/Blender/smoke_2.png'),
+            mist1: require('./Projects/Blender/mist_1.png'),
+            mist2: require('./Projects/Blender/mist_2.png'),
+            rain1: require('./Projects/Blender/rain_1.png'),
+            rain2: require('./Projects/Blender/rain_2.png'),
+            skyDome1: require('./Projects/Blender/sky_dome_1.png'),
+            skyDome2: require('./Projects/Blender/sky_dome_2.png'),
+            skyDome3: require('./Projects/Blender/sky_dome_3.png'),
+            skyDome4: require('./Projects/Blender/sky_dome_4.png'),
+        },
+    },
+
+    hobbies: {
+        dance: {
+            card: require('./Hobbies/Dance/card.jpg'),
+            lgcyLogo: require('./Hobbies/Dance/lgcy_logo.png'),
+            xtrmLogo: require('./Hobbies/Dance/xtrm_logo.png'),
+            coLogo: require('./Hobbies/Dance/co_logo.png'),
+        },
+        music: {
+            card: require('./Hobbies/Music/card.jpeg'),
+            lsjumb_logo: require('./Hobbies/Music/lsjumb_logo.jpg'),
+            band1: require('./Hobbies/Music/band1.jpg'),
+            band2: require('./Hobbies/Music/band2.jpg'),
+            band3: require('./Hobbies/Music/band3.jpeg'),
+            band4: require('./Hobbies/Music/band4.jpg'),
+            band5: require('./Hobbies/Music/band5.jpg'),
+            band6: require('./Hobbies/Music/band6.jpg'),
+            band7: require('./Hobbies/Music/band7.jpg'),
+            band8: require('./Hobbies/Music/band8.jpg'),
+            band9: require('./Hobbies/Music/band9.png'),
+            band10: require('./Hobbies/Music/band10.jpg'),
+            band11: require('./Hobbies/Music/band11.jpg'),
+        },
+        rockets: {
+            card: require('./Hobbies/Rockets/card.jpg'),
+            ssi_logo: require('./Hobbies/Rockets/ssi_logo.png'),
+            ssi1: require('./Hobbies/Rockets/ssi1.jpg'),
+            ssi2: require('./Hobbies/Rockets/ssi2.jpg'),
+        },
+        videoEditing: {
+            card: require('./Hobbies/VideoEditing/card.png'),
+        }
+    }
+}
+
+export default images;
